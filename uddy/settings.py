c318b205acdf415eb59d4b54bfa2c08b704e6ba6
amorgrace <akanforte@gmail.com>
2025-08-19 13:57:18 +0100

    cors updated

--- uddy/settings.py
+++ uddy/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 SIMPLE_JWT = {
