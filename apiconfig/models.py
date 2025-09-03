1937908bcfad4f31879d201fd42fed0d3eecfff5
amorgrace <akanforte@gmail.com>
2025-09-03 23:56:52 +0100

    kora checjut

--- apiconfig/models.py
+++ apiconfig/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.conf import settings
 from decimal import Decimal
-
+import uuid
 
 
 class CustomUserManager(BaseUserManager):
@@ -83,7 +83,7 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"))
     status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='pending')
-    reference = models.CharField(max_length=100, unique=True, default='')  # for Kora
+    reference = models.CharField(max_length=100, unique=True, default=uuid.uuid4)  # for Kora
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
